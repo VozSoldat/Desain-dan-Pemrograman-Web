47e6b63ce1e6d70fc143bbcfd1651efa13f9cb4a
Muhammad Erril <errilputra@gmail.com>
2024-09-17 09:30:29 +0700

    praktikum3: membuat operasi aritmatika (no3.1)

---
+++ dasarWeb-Erril/Praktikum4/operator.php
@@ -0,0 +1,21 @@
+<?php 
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Variabel a: {$a} <br>";
+echo "Variabel b: {$b} <br>";
+echo "Penjumlahan: {$hasilTambah} <br>";
+echo "Pengurangan: {$hasilKurang} <br>";
+echo "Perkalian: {$hasilKali} <br>";
+echo "Pembagian: {$hasilBagi} <br>";
+echo "Sisa Bagi: {$sisaBagi} <br>";
+echo "Pangkat: {$pangkat} <br>";
+
+?>
