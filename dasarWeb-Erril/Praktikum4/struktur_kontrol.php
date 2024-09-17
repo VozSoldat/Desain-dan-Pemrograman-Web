379f761eb62ce40595d5a9e59229531ade67665b
Muhammad Erril <muhamaderril7@gmail.com>
2024-09-17 12:24:09 +0700

    praktikum4: membuat if bersarang dalam foreach (no4.5)

--- dasarWeb-Erril/Praktikum4/struktur_kontrol.php
+++ dasarWeb-Erril/Praktikum4/struktur_kontrol.php
@@ -50,4 +50,15 @@ foreach ($skorUjian as $skor) {
 
 echo "Total skor ujian adalah: $totalSkor";
 
+echo "<br><br>";
+
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai < 60) {
+        echo "Nilai: $nilai (Tidak lulus)<br>";
+        continue;
+    }else echo "Nilai: $nilai (Lulus)<br>";
+}
+
 ?>
