a46fb542d5345dc49e8d129aed166973140f537d
VozSoldat <muhamaderril7@gmail.com>
2024-10-01 08:54:23 +0700

    Bagian3: membuat fungsi yang dipanggil di dalam fungsi lain (no8)

--- dasarWeb-Erril/Praktikum6/praktik_php/praktik.php
+++ dasarWeb-Erril/Praktikum6/praktik_php/praktik.php
@@ -1,11 +1,18 @@
 <?php 
-// no4
 // membuat fungsi
-function hitungMundur($thn_lahir, $thn_sekarang){
+function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
 
-echo "Umur saya adalah ". hitungMundur(2005, 2024) . "tahun";
+function perkenalan ($nama, $salam="Assalamualaikum"){
+    echo $salam.",";
+    echo "Perkenalkan, nama saya " . $nama . " <br>";
+
+    // memanggil fungsi lain
+    echo "Saya berusia ". hitungUmur(2005, 2024) . " tahun <br>";
+    echo "Senang berkenalan dengan anda <br>";
+}
+perkenalan("Erril");
 
 ?>
