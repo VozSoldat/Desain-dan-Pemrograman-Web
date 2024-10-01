496fda218d92948b51bc295b787e48eb00cb62b8
VozSoldat <muhamaderril7@gmail.com>
2024-10-01 08:56:40 +0700

    Bagian3: membuat fungsi rekursif untuk menampilkan angka (no10)

--- dasarWeb-Erril/Praktikum6/praktik_php/rekursif.php
+++ dasarWeb-Erril/Praktikum6/praktik_php/rekursif.php
@@ -1,8 +1,14 @@
 <?php 
-function tampilkanHaloDunia(){
-    echo "Halo Dunia <br>";
-    tampilkanHaloDunia();
+for ($i=1; $i <=25 ; $i++) { 
+    echo "Perulangan ke-{$i} <br>";
 }
 
-tampilkanHaloDunia();
+function tampilkanAngka(int $jumlah, int $indeks = 1){
+    echo "Perulangan ke-{$indeks} <br>";
+    // panggil diri sendiri selama $indeks <= $jumlah
+    if($indeks < $jumlah){
+        tampilkanAngka($jumlah, $indeks+1);
+    }
+}
+tampilkanAngka(20);
 ?>
