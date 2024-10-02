b3fbcd40297e21f4b9a3aae29aabe486dca24a6c
VozSoldat <muhamaderril7@gmail.com>
2024-10-02 19:36:06 +0700

    tugas no 1

--- dasarWeb-Erril/Praktikum6/array_1.php
+++ dasarWeb-Erril/Praktikum6/array_1.php
@@ -20,5 +20,20 @@
             echo $Listdosen[$i] ."<br>";
         }
         ?>
+
+        <!-- dengan perulangan -->
+        <?php 
+        $Listdosen=["Elok Nur Hamdana","Unggul Pamenang","Bagas Nugraha"];
+
+        echo $Listdosen[2] . "<br>";
+        echo $Listdosen[0] ."<br>";
+        echo $Listdosen[1] ."<br>";
+
+        echo "<br>";
+
+        for ($i=0; $i < count($Listdosen); $i++) { 
+            echo $Listdosen[$i] ."<br>";
+        }
+        ?>
     </body>
 </html>
