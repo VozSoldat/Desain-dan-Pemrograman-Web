7d412f74730e1ff378632e5f504dde848b943d46
VozSoldat <muhamaderril7@gmail.com>
2024-10-08 08:17:16 +0700

    no1.2: menggunakan isset untuk array

--- dasarWeb-Erril/Praktikum7/isset.php
+++ dasarWeb-Erril/Praktikum7/isset.php
@@ -7,4 +7,10 @@ if (isset($umur) && $umur >= 18) {
     echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan";
 }
 
-
+echo "<br><br>";
+$data = array("nama"=>"Jane", "usia"=>25);
+if (isset($data["usia"])) {
+    echo "Nama: ". $data["nama"] ."<br>";
+}else{
+    echo "Variabel 'nama' tidak ditemukan dalam array.<br>";
+}
