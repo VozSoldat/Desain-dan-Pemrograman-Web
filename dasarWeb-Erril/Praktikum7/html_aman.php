4e18651baba6bbae3d6ca2209e529091445c1c28
VozSoldat <muhamaderril7@gmail.com>
2024-10-10 06:47:31 +0700

    no4.2: membuat inout email dengan aman

--- dasarWeb-Erril/Praktikum7/html_aman.php
+++ dasarWeb-Erril/Praktikum7/html_aman.php
@@ -8,15 +8,37 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
     $input= $_POST['input'];
     $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
     
-    echo $input;
+    echo "$input <br><br>";
+
+    $email= "";
+    $email = $_POST['email'];
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Email is valid: $email";
+    }else {
+        echo "Email is not valid";
+    }
 }
 ?>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <label for="input">Input:</label>
         <input type="text" name="input" id="input">
+<br>
+<br>
+
+        <label for="email">Email:</label>
+        <input type="text" name="email" id="email">
+<br>
+
         <input type="submit">
     </form>
 
+    <br><br>
+
+    <!-- <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <label for="email">Email:</label>
+        <input type="text" name="email" id="email">
+        <input type="submit">
+    </form> -->
 
 </body>
 </html>
