12d376158ee1c0341c1a2ccbb588a444b7315156
VozSoldat <muhamaderril7@gmail.com>
2024-10-10 20:01:47 +0700

    no7.2: membuat validasi dengan jquery

--- dasarWeb-Erril/Praktikum7/form_validasi.php
+++ dasarWeb-Erril/Praktikum7/form_validasi.php
@@ -17,6 +17,34 @@
         
         <input type="submit" name="submit" value="Submit">
     </form>
+
+    <script>
+        $(document).ready(function() {
+            $("#myForm").submit(function(event) {
+                var nama = $("#nama").val();
+                var email = $("#email").val();
+                var valid = true;
+
+                if (nama ==="") {
+                    $("#nama-error").text("Nama harus diisi");
+                    valid = false;
+                }else{
+                    $("#nama-error").text("");
+                }
+
+                if (email ==="") {
+                    $("#email-error").text("Email harus diisi");
+                    valid = false;
+                }else{
+                    $("#email-error").text("");
+                }
+
+                if (!valid) {
+                    event.preventDefault();
+                }
+            });
+        })
+    </script>
 </body>
 
 </html>
