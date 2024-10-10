0206f664c60c992cdc7a703a170e5499cceec864
VozSoldat <muhamaderril7@gmail.com>
2024-10-10 07:07:38 +0700

    no5.2: menggunakan function pencocokan

--- dasarWeb-Erril/Praktikum7/regex.php
+++ dasarWeb-Erril/Praktikum7/regex.php
@@ -4,12 +4,13 @@
 <body>
     
 <?php 
-$pattern = '/[a-z]/';
-$text = 'This is a sample text.';
-if (preg_match($pattern, $text)) {
-    echo "Huruf kecil ditemukan";
+// $pattern = '/[a-z]/';
+$pattern = '/[0-9]+/';
+$text = 'There are 123 apples.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: $matches[0]";
 }else{
-    echo "Huruf kecil tidak ditemukan";
+    echo "Tidak ada yang cocok!";
 }
 ?>
 </body>
