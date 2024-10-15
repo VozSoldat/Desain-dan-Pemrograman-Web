eac6a9d7f5255ff3d83801f95c1f2fab865ecbea
VozSoldat <muhamaderril7@gmail.com>
2024-10-15 08:50:23 +0700

    no2: mengupload file ke dir uploads/

--- dasarWeb-Erril/Praktikum8/upload.php
+++ dasarWeb-Erril/Praktikum8/upload.php
@@ -3,7 +3,7 @@ if (isset($_POST['submit'])) {
     $targetdir = "uploads/"; // direktori tujuan untuk menyimpan file
     $targetfile = $targetdir . basename($_FILES['myfile']['name']);
 
-    if (move_uploaded_file($_FILES['myfiles']['tmp_name'], $targetfile)) {
+    if (move_uploaded_file($_FILES['myfile']['tmp_name'], $targetfile)) {
         echo "File berhasil diunggah";
     } else {
         echo "File gagal diunggah";
