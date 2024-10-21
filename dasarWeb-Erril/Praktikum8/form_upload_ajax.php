dd6b0cc86de113a199961b12ee247454ff410bf2
VozSoldat <muhamaderril7@gmail.com>
2024-10-21 20:43:10 +0700

    no12: membuat css

--- dasarWeb-Erril/Praktikum8/form_upload_ajax.php
+++ dasarWeb-Erril/Praktikum8/form_upload_ajax.php
@@ -8,14 +8,15 @@
 <body>
     <div class="upload-form-container">
         <h2>Unggah File Dokumen</h2>
-        <form action="upload.php" id="upload-form" method="post" enctype="multipart/form-data"></form>
-        <div class="file-input-container">
-            <input type="file" name="file" id="file" class="file-input">
-            <label for="file" class="file-label">Pilih File</label>
-        </div>
-        <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>
-            Unggah
-        </button>
+        <form action="upload.php" id="upload-form" method="post" enctype="multipart/form-data">
+            <div class="file-input-container">
+                <input type="file" name="file" id="file" class="file-input">
+                <label for="file" class="file-label">Pilih File</label>
+            </div>
+            <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>
+                Unggah
+            </button>
+        </form>
         <div id="status" class="upload-status"></div>
     </div>
 
