9fbe6c81aee920fbf910265a9f50e97659600ee7
VozSoldat <muhamaderril7@gmail.com>
2024-10-22 08:31:40 +0700

    no20: membuat session

--- dasarWeb-Erril/Praktikum8/sessionCall.php
+++ dasarWeb-Erril/Praktikum8/sessionCall.php
@@ -6,7 +6,9 @@ session_start();
 <html>
     <body>
         <?php 
-        echo""
+        echo"Favorite color is {$_SESSION["favcolor"]} <br>";
+        echo"Favorite animal is {$_SESSION["favanimal"]}. <br>";
+        
         ?>
     </body>
 </html>
